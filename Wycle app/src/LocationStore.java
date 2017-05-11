5578845a7598bcfefa9a612d6f5032734db5ca09
Andrew <theaiaj.general@gmail.com>
2017-05-11 14:19:44 +0100

    IDE setup

---
+++ Wycle app/src/LocationStore.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class LocationStore {
+
+    //TODO: Store all the cities which can be used for locations and make them easily searchable.
+
+    // Store cities
+    private static ArrayList<String> cities;
+    // Singleton class so store the current instance
+    private static LocationStore instance;
+
+    private LocationStore() {
+        cities = new ArrayList<>();
+        readInCities();
+    }
+
+    public static LocationStore getInstance() {
+        if (null == instance){
+            instance = new LocationStore();
+            return instance;
+        }
+        return instance;
+    }
+
+    private void readInCities() {
+
+        String fileName = "cities.txt";
+
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            stream.forEach(c -> cities.add(c));
+        } catch (IOException e) {
+            //TODO: change the handle
+            System.out.println("File not here");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        LocationStore ls = getInstance();
+        for (String s : cities) {
+            System.out.println(s);
+        }
+    }
+}
