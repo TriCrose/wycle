77ab468b65c076af7c65f8e800f772e97de9ffce
Andrew <theaiaj.general@gmail.com>
2017-05-16 13:50:22 +0100

    Added grid layouts and methods to draw areas of app. Added DetailedRow class to hold row data for detailed outlook.

---
+++ src/ui/DetailedRow.java
@@ -0,0 +1,36 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DetailedRow {
+
+    private JPanel row;
+
+
+    public DetailedRow() {
+
+    }
+
+
+    public JPanel getPanel() {
+
+        row = new JPanel(new GridLayout(1, 0));
+
+        JLabel labelTime = new JLabel("time");
+        JLabel labelrain = new JLabel("rain");
+        JLabel labeltemp = new JLabel("temp");
+        JLabel labelwind = new JLabel("wind");
+        JLabel labelicon = new JLabel("icon");
+
+        row.add(labelTime);
+        row.add(labelrain);
+        row.add(labeltemp);
+        row.add(labelwind);
+        row.add(labelicon);
+
+        row.setBackground(new Color(138, 192, 239));
+
+        return row;
+    }
+}
--- src/ui/MainWindow.java
+++ src/ui/MainWindow.java
@@ -5,38 +5,44 @@ import javax.swing.border.EtchedBorder;
 import java.awt.*;
 
 public class MainWindow extends JFrame {
-	private static final long serialVersionUID = 1L;
-	private static final int WIDTH = 394;
-	private static final int HEIGHT = 700;
-	private static final Color backColour = new Color(138, 192, 239);
+
+    private static final long serialVersionUID = 1L;
+    private static final int WIDTH = 394;
+    private static final int HEIGHT = 700;
+    private static final Color backColour = new Color(138, 192, 239);
     private static GridBagConstraints constraints = new GridBagConstraints();
+
     private JPanel day;
-	private JPanel icons;
-	private JPanel rainWind;
-	private JPanel detailed;
-	
-	public MainWindow() {
-		super("Wycle");
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setResizable(false);
-		setSize(WIDTH, HEIGHT);
-		setLocationRelativeTo(null);
-		setLayout(new GridBagLayout());
-		getContentPane().setBackground(backColour);
+    private JPanel icons;
+    private JPanel rainWind;
+    private JPanel detailed;
 
-		day = addPanel(0, 0.04);
-		JLabel labelDay = new JLabel("Today");
-		labelDay.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
-		labelDay.setHorizontalAlignment(JLabel.CENTER);
-		day.add(labelDay);
 
-		icons = addPanel(1, 0.1);
-        rainWind = addPanel2(2, 0.1);
+    public MainWindow() {
+
+        super("Wycle");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setResizable(false);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setLayout(new GridBagLayout());
+        getContentPane().setBackground(backColour);
+
+        day = addPanelBorderLayout(0, 0.04);
+        JLabel labelDay = new JLabel("Today");
+        labelDay.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
+        labelDay.setHorizontalAlignment(JLabel.CENTER);
+        day.add(labelDay);
+
+        icons = addPanelGridLayout(1, 0.1, 1, 2);
+        drawIcons();
+        rainWind = addPanelGridLayout(2, 0.1, 1, 3);
         drawRainWind();
-        detailed = addPanel(3, 0.5);
+        detailed = addPanelGridLayout(3, 0.5, 0, 1);
+        drawDetailed();
 
         setVisible(true);
-	}
+    }
 
 
     public static void main(String args[]) {
@@ -45,25 +51,8 @@ public class MainWindow extends JFrame {
     }
 
 
-	private JPanel addPanel(int gridY, double weight) {
-		JPanel panel = new JPanel(new BorderLayout());
-		panel.setBackground(backColour);
-		panel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+    private JPanel addPanel(JPanel panel, int gridY, double weight) {
 
-        constraints.fill = GridBagConstraints.BOTH;
-		constraints.gridx = 0;
-		constraints.gridy = gridY;
-		constraints.weightx = 1.0;
-		constraints.weighty = weight;
-
-        add(panel, constraints);
-		return panel;
-	}
-
-
-    private JPanel addPanel2(int gridY, double weight) {
-
-        JPanel panel = new JPanel(new GridLayout(1, 3));
         panel.setBackground(backColour);
         panel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
 
@@ -78,6 +67,32 @@ public class MainWindow extends JFrame {
     }
 
 
+    private JPanel addPanelBorderLayout(int gridY, double weight) {
+
+        JPanel panel = new JPanel(new BorderLayout());
+        return addPanel(panel, gridY, weight);
+    }
+
+
+    private JPanel addPanelGridLayout(int gridY, double weight, int rows, int columns) {
+
+        JPanel panel = new JPanel(new GridLayout(rows, columns));
+        return addPanel(panel, gridY, weight);
+    }
+
+
+    private void drawIcons() {
+
+        JLabel labelBikeCoefficient = new JLabel("bike coefficient");
+        JLabel labelWeatherIcon = new JLabel("weather icon");
+
+        labelBikeCoefficient.setHorizontalAlignment(JLabel.CENTER);
+        labelWeatherIcon.setHorizontalAlignment(JLabel.CENTER);
+
+        icons.add(labelBikeCoefficient);
+        icons.add(labelWeatherIcon);
+    }
+
     private void drawRainWind() {
 
         JLabel labelRain = new JLabel("Rain");
@@ -91,5 +106,14 @@ public class MainWindow extends JFrame {
         rainWind.add(labelRain);
         rainWind.add(labelTemp);
         rainWind.add(labelWind);
+    }
+
+
+    private void drawDetailed() {
+
+        for (int i = 0; i < 10; i++) {
+            DetailedRow dr = new DetailedRow();
+            detailed.add(dr.getPanel());
+        }
     }
 }
