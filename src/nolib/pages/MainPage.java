85b8f07bbb05facd8c9786c99be81e2d5b1e2b74
Tristram Newman <tristram.newman@gmail.com>
2017-05-22 14:18:23 +0100

    Removed references to non-existent resources

--- src/nolib/pages/MainPage.java
+++ src/nolib/pages/MainPage.java
@@ -1,23 +1,14 @@
 package nolib.pages;
 
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
 import nolib.*;
 
 public class MainPage implements AppPage {
 	private String day;
-	private BufferedImage compass, bike;
 	
 	public MainPage() {
 		day = "Today";
-		try { 
-			compass = ImageIO.read(new File("img/compass.png"));
-			bike = ImageIO.read(new File("img/bike.png"));
-		} catch (IOException e) { e.printStackTrace(); }
 	}
 	
 	@Override
@@ -29,9 +20,7 @@ public class MainPage implements AppPage {
 	public void render(Graphics2D g) {
 		g.setFont(AppPage.DAY_FONT);
 		AppPage.drawString(g, day, AppWindow.width()/2, 60);
-		g.drawImage(compass, AppWindow.width() - compass.getWidth() - 5, 5, null);
 		g.drawLine(0, 100, AppWindow.width(), 100);
-		g.drawImage(bike, 0, 100, null);
 	}
 	
 	@Override
